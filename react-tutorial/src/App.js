5b5a78b043a29f45678d5b2871c154f048d20d69
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-01 15:02:46 -0300

    Adereços

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,15 +1,34 @@
-// 
+//
 import React, { Component } from "react";
-import Table from './Table'
+import Table from "./Table";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="container">
-                <Table />
-            </div>
-        )
-    }
+  render() {
+    const characters = [
+      {
+        name: "Charlie",
+        job: "Janitor",
+      },
+      {
+        name: "Mac",
+        job: "Bouncer",
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+    },
+    {
+        name: 'Dennis',
+        job: 'Bartender',
+    },
+    ];
+
+    return (
+      <div className="container">
+        <Table characterData = {characters} />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
