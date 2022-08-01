950dd55fc6c0afbeee04f2949eb12981324ebbc3
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-01 14:18:18 -0300

    Componentes simples

--- react-tutorial/src/Table.js
+++ react-tutorial/src/Table.js
@@ -1,43 +1,52 @@
-// 
+//
 import React, { Component } from "react";
 
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  );
+};
+
+const TableBody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>Charlie</td>
+        <td>Janitor</td>
+      </tr>
+
+      <tr>
+        <td>Mac</td>
+        <td>Bouncer</td>
+      </tr>
+
+      <tr>
+        <td>Dee</td>
+        <td>Aspiring actress</td>
+      </tr>
+
+      <tr>
+        <td>Dennis</td>
+        <td>Bartender</td>
+      </tr>
+    </tbody>
+  );
+};
+
 class Table extends Component {
-    render() {
-      return (
-        <table>
-          
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Job</th>
-            </tr>
-          </thead>
-          
-          <tbody>
-            <tr>
-              <td>Charlie</td>
-              <td>Janitor</td>
-            </tr>
-        
-            <tr>
-              <td>Mac</td>
-              <td>Bouncer</td>
-            </tr>
-        
-            <tr>
-              <td>Dee</td>
-              <td>Aspiring actress</td>
-            </tr>
-        
-            <tr>
-              <td>Dennis</td>
-              <td>Bartender</td>
-            </tr>
-          </tbody>
-        
-        </table>
-      )
-    }
+  render() {
+    return (
+      <table>
+        <TableHeader />
+        <TableBody />
+      </table>
+    );
   }
-  
-  export default Table
+}
+
+export default Table;
